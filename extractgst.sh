529b33ec7f0d8dff484bcba7f65620e7323a9c15
Andreas Gerlach <andi@appelgriebsch.org>
2021-02-17 10:23:55 +0100

    initial commit of GNOME base shell theme

---
+++ extractgst.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+gst=gnome-shell-theme.gresource
+workdir=${PWD}
+
+for r in `gresource list $gst`; do
+	r=${r#\/org\/gnome\/shell/}
+	if [ ! -d $workdir/${r%/*} ]; then
+	  mkdir -p $workdir/${r%/*}
+	fi
+done
+
+for r in `gresource list $gst`; do
+        gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
+done
